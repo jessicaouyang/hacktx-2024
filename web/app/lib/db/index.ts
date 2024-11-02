07a4941be3c90bd494bb210f235f561b77c2ab2b
Dhruv Arora <dhruvarora.us@gmail.com>
2024-11-02 12:31:00 -0500

    remove schema for now

--- web/app/lib/db/index.ts
+++ web/app/lib/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { sql } from "@vercel/postgres";
 
-import * as schema from "@lib/db/schema";
+// import * as schema from "@lib/db/schema";
 
-export const db = drizzle(sql, { schema });
+// export const db = drizzle(sql, { schema });
+export const db = drizzle(sql);
