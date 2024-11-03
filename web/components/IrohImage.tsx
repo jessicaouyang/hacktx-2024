cce59582c5653eb2f80e86e889958723a9f8412b
Dhruv Arora <dhruvarora.us@gmail.com>
2024-11-02 19:32:19 -0500

    basic thing

--- web/components/IrohImage.tsx
+++ web/components/IrohImage.tsx
@@ -4,14 +4,11 @@ import Image from "next/image";
 export default function IrohImage({ response }: { response?: Message }) {
   return (
     <>
-      <Image
-        alt={"Iroh"}
-        height={1000}
-        width={1000}
-        src={"/uncle-iroh-avatar.webp"}
-        className="h-96 w-fit mx-auto"
-      />
+    <div  className="relative h-96 w-96 mx-auto bg-[url('/uncle-iroh-avatar.webp')] bg-contain bg-no-repeat bg-center">
+      <div className="absolute -right-80 w-96 bottom-1/2">
       {response?.content}
+      </div>
+    </div>
     </>
   );
 }
