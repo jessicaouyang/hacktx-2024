ce15cafc2c73c6e913ccb441be456f7859322289
Dhruv Arora <dhruvarora.us@gmail.com>
2024-11-02 21:25:51 -0500

    remove extra fonts

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,19 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 const font = FontSans({ subsets: ["latin"] });
 
