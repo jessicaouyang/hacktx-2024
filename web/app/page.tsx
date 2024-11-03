0eb0f223417e2dca2e3b42e5b32ffe5d51b5fca4
Vrishank Viswanath <vrishank@utexas.edu>
2024-11-02 20:54:02 -0500

    moved path of app

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,13 +1,5 @@
 import IrohChat from "@/components/IrohChat";
 
 export default function Home() {
-  return (
-    <div className='bg-[#FFECD1] h-screen flex flex-col gap-4'>
-      <div className=' bg-[#887357] rounded-b-3xl h-24'></div>
-      <div className='flex-1'>
-        <IrohChat />
-      </div>
-      <div className=' bg-[#738857] rounded-t-3xl h-16'></div>
-    </div>
-  );
+  return <div className='bg-[#FFECD1] h-screen flex flex-col gap-4'>You landed blud</div>;
 }
