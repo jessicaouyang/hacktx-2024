4395c9b91e3eda836e446a07e4055759a812325c
Dhruv Arora <dhruvarora.us@gmail.com>
2024-11-02 21:54:41 -0500

    decrease iroh bubble height

--- web/components/IrohImage.tsx
+++ web/components/IrohImage.tsx
@@ -14,7 +14,7 @@ export default function IrohImage({ response }: { response?: Message }) {
     <div className="relative h-96 w-96 mx-auto bg-[url('/uncle-iroh-avatar.webp')] bg-contain bg-no-repeat bg-center">
       <div
         ref={messageRef}
-        className='absolute -right-80 w-96 bottom-1/2 bg-white border-[1.5px] border-[#AB7D4C] rounded-3xl rounded-bl-sm p-3 max-h-64 overflow-y-scroll'
+        className='absolute -right-80 w-96 bottom-1/2 bg-white border-[1.5px] border-[#AB7D4C] rounded-3xl rounded-bl-sm p-3 max-h-48 overflow-y-scroll'
       >
         {response?.content ??
           "How is your spirit today? Perhaps a listening ear can help calm the waters of your mind."}
